fa308745a1223beadd602c3dfbf81fd48977bf7d
xtribu02 <tribula.tomas@gmail.com>
2020-04-12 12:20:45 +0200

    continuing on checking instructions

--- intlib/XmlParser.py
+++ intlib/XmlParser.py
@@ -9,7 +9,8 @@ class XmlParser:
         self.source = source
         self.instrList = instr_list
 
-    def xml_check(self):
+    def parse(self):
+        # -----------XML Syntax---------------
         try:
             tree = ET.parse(self.source)
             self.root = tree.getroot()
@@ -24,15 +25,15 @@ class XmlParser:
             # TODO: ERROR root element nema program
             exit(31)
 
-        for attrib in self.root.attrib:
-            if attrib not in ['language', 'name', 'description']:
+        for attr in self.root.attr:
+            if attr not in ['language', 'name', 'description']:
                 # TODO: ERROR nepovolene atributy <program>
                 exit(31)
 
-        if 'language' not in self.root.attrib:
+        if 'language' not in self.root.attr:
             # TODO: ERROR neni language v <program>
             exit(31)
-        if str(self.root.attrib['language']).lower() is not 'ippcode20':
+        if str(self.root.attr['language']).lower() is not 'ippcode20':
             # TODO: ERROR language neni ippcode20
             exit(31)
 
@@ -41,24 +42,24 @@ class XmlParser:
             if instr.tag is not 'instruction':
                 # TODO: ERROR spatny nazev elementu instr
                 exit(31)
-            if 'opcode' not in instr.attrib:
+            if 'opcode' not in instr.attr:
                 # TODO: ERROR opcode neni v elementu instr
                 exit(31)
-            if 'order' not in instr.attrib:
+            if 'order' not in instr.attr:
                 # TODO: ERROR order neni v elementu instr
                 exit(31)
-            instr_order.append(instr.attrib['order'])
+            instr_order.append(instr.attr['order'])
 
             arg_count = 0
             for arg in instr:
                 arg_count += 1
-                if arg.tag is not 'arg'+str(arg_count):
+                if arg.tag is not 'arg' + str(arg_count):
                     # TODO: ERROR arg bad name
                     exit(31)
-                if 'type' not in arg.attrib:
+                if 'type' not in arg.attr:
                     # TODO: ERROR type neni
                     exit(31)
-                if arg.attrib['type'] not in ['int', 'bool', 'string', 'nil', 'label', 'type', 'var']:
+                if arg.attr['type'] not in ['int', 'bool', 'string', 'nil', 'label', 'type', 'var']:
                     # TODO: ERROR bad type
                     exit(31)
             for i in instr_order:
@@ -68,3 +69,87 @@ class XmlParser:
             if len(set(instr_order)) is not len(instr_order):
                 # TODO: ERROR duplicitni order number
                 exit(31)
+
+        # -----------Kontrola instrukci---------------
+        for instr in self.root:
+            if instr.attr['opcode'] in ['CREATEFRAME', 'PUSHFRAME', 'POPFRAME', 'RETURN', "BREAK"]:
+                if len(list(instr)) is not 0:
+                    # TODO: ERROR nespravny pocet argumentu instrukce
+                    exit(31)
+                self.instrList.insert_instr(Instr(instr.attr['opcode'], arg1=instr[0]))
+            elif instr.attr['opcode'] in ['DEFVAR', 'POPS']:
+                if len(list(instr)) is not 1:
+                    # TODO: ERROR nespravny pocet argumentu instrukce
+                    exit(31)
+                self.check_variable(instr[0])
+                self.instrList.insert_instr(Instr(instr.attr['opcode'], arg1=instr[0]))
+            elif instr.attr['opcode'] in ['CALL', 'LABEL', 'JUMP']:
+                if len(list(instr)) is not 1:
+                    # TODO: ERROR nespravny pocet argumentu instrukce
+                    exit(31)
+                self.check_label(instr[0])
+                self.instrList.insert_instr(Instr(instr.attr['opcode'], arg1=instr[0]))
+            elif instr.attr['opcode'] in ['PUSHS', 'WRITE', 'EXIT', 'DPRINT']:
+                if len(list(instr)) is not 1:
+                    # TODO: ERROR nespravny pocet argumentu instrukce
+                    exit(31)
+                self.check_symbol(instr[0])
+                self.instrList.insert_instr(Instr(instr.attr['opcode'], arg1=instr[0]))
+            elif instr.attr['opcode'] in ['MOVE', 'INT2CHAR', 'STRLEN', 'TYPE', 'NOT']:
+                if len(list(instr)) is not 2:
+                    # TODO: ERROR nespravny pocet argumentu instrukce
+                    exit(31)
+                self.check_variable(instr[0])
+                self.check_symbol(instr[1])
+                self.instrList.insert_instr(Instr(instr.attr['opcode'], arg1=instr[0], arg2=instr[1]))
+
+    def check_variable(self, variable):
+        if variable.attr['type'] is not 'var':
+            # TODO: ERROR wrong atribut promenne
+            exit(52)
+        if variable.value is None or not re.match('^(GF|LF|TF)@[a-zA-Z_\-$&%*!?][\w_\-$&%*!?]*$', variable.value):
+            # TODO: ERROR bad value of var
+            exit(32)
+
+    def check_symbol(self, symbol):
+        if symbol.attr['type'] is 'var':
+            self.check_variable(symbol)
+        elif symbol.attr['type'] is 'int':
+            if symbol.value is None or not re.match('^([+-]?[1-9][0-9]*|[+-]?(0-9))$', symbol.value):
+                # TODO: ERROR int neni cislo
+                exit(32)
+        elif symbol.attr['type'] is 'bool':
+            if symbol.value is None or symbol.value in ['true', 'false']:
+                # TODO: ERROR bool neni true/false
+                exit(32)
+        elif symbol.attr['type'] is 'nil':
+            if symbol.value is None or symbol.value is not 'nil':
+                # TODO: ERROR nil nema nil
+                exit(32)
+        elif symbol.attr['type'] is 'string':
+            if symbol.value is None:
+                symbol.value = ''
+                return
+            if not re.search('^(\\\\[0-9]{3}|[^\s\\\\#])*$', symbol.value):
+                # TODO: ERROR bad string
+                exit(32)
+            # TODO: STRING escape sequence
+        else:
+            # TODO: ERROR symbol neni var/int/bool/nil/string
+            exit(52)
+
+    def check_label(self, label):
+        if label.attr['type'] is not 'label':
+            # TODO: ERROR typ neni label
+            exit(52)
+        if label.value is None or not re.match('^[a-zA-Z_\-$&%*!?][\w_\-$&%*!?]*', label.value):
+            # TODO: ERROR bad label
+            exit(32)
+
+    def check_type(self, type):
+        if type.attr['type'] is not 'type':
+            # TODO: ERROR chybny atribut type
+            exit(52)
+        if type.attr['type'] is None or not re.match('^(int|bool|nil|string)$', type.value):
+            # TODO: ERROR chybny typ
+            exit(32)
